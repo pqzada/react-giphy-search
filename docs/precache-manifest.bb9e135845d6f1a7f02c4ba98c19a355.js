9ad414a3a08853af6be96901ad9ec9096f47abaf
pqzada <pqzada@gmail.com>
2022-05-30 22:01:03 -0400

    updates docs

---
+++ docs/precache-manifest.bb9e135845d6f1a7f02c4ba98c19a355.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "463be18a826b0cee845e",
+    "url": "/static/css/main.6aa47adf.chunk.css"
+  },
+  {
+    "revision": "463be18a826b0cee845e",
+    "url": "/static/js/main.463be18a.chunk.js"
+  },
+  {
+    "revision": "6e6e13cb0fe0c4e39585",
+    "url": "/static/js/1.6e6e13cb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2814652cab13c84c2d268429882b6138",
+    "url": "/index.html"
+  }
+];
